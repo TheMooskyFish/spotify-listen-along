23ebe120e8ea00ec33abf841a444a9d62a4a688a
TheMooskyFish <TheMooskyFish@gmail.com>
2022-12-16 00:07:04 +0300

    First Changes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import { Injector, webpack } from "replugged";
 import { AnyFunction } from "replugged/dist/types/util";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const typingMod = (await webpack.waitForModule(webpack.filters.byProps("startTyping"))) as {
-    startTyping: AnyFunction;
-  };
-  const getChannelMod = (await webpack.waitForModule(webpack.filters.byProps("getChannel"))) as {
-    getChannel: (id: string) => {
-      name: string;
-    };
+  const spotify = (await webpack.waitForModule(
+    webpack.filters.byProps("getActiveSocketAndDevice"),
+  )) as {
+    getActiveSocketAndDevice: AnyFunction;
   };
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel as string);
-      console.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
+  if (spotify) {
+    inject.after(spotify, "getActiveSocketAndDevice", ([_, ret]) => {
+      if (ret) (ret as any).socket.isPremium = true;
+      return ret;
     });
   }
 }

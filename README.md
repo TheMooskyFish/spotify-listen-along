b19e64a8e2ddf1b818fc894bcd43bfa898f2474e
TheMooskyFish <TheMooskyFish@gmail.com>
2022-12-18 23:50:47 +0300

    Version 0.0.4

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 This is a plugin made for [Replugged](https://github.com/replugged-org/replugged)
 
 ## Installation
-`Download Latest Version`  
-`Copy it to %APPDATA%/replugged/plugins`  
-`Restart Discord`  
+Download latest version then copy it
+- Windows: `%APPDATA%/replugged/plugins`
+- macOS: `~/Library/Application Support/replugged/plugins`
+- Other: `$XDG_CONFIG_HOME/replugged/plugins` or `~/.config/replugged/plugins`   
+- Restart Discord
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 import { Injector, webpack } from "replugged";
 import { AnyFunction } from "replugged/dist/types/util";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
-  const spotify = (await webpack.waitForModule(
-    webpack.filters.byProps("getActiveSocketAndDevice"),
-  )) as {
+  const spotify = await webpack.waitForModule<{
     getActiveSocketAndDevice: AnyFunction;
-  };
+  }>(webpack.filters.byProps("getActiveSocketAndDevice"));
 
   if (spotify) {
     inject.after(spotify, "getActiveSocketAndDevice", (_, ret) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       if (ret) (ret as any).socket.isPremium = true;
       return ret;
     });

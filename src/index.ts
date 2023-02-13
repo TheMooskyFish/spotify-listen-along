b40e088e21829c9ed8d83aa645eb207619719444
TheMooskyFish <TheMooskyFish@gmail.com>
2023-02-13 21:38:14 +0300

    Final Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { Injector, webpack } from "replugged";
-import { AnyFunction } from "replugged/dist/types/util";
 
 const inject = new Injector();
 
 export async function start(): Promise<void> {
   const spotify = await webpack.waitForModule<{
-    getActiveSocketAndDevice: AnyFunction;
+    getActiveSocketAndDevice: () => void;
   }>(webpack.filters.byProps("getActiveSocketAndDevice"));
-
+  interface spotifysocket {
+    isPremium: boolean;
+  }
   if (spotify) {
-    inject.after(spotify, "getActiveSocketAndDevice", (_, ret) => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      if (ret) (ret as any).socket.isPremium = true;
+    inject.after(spotify, "getActiveSocketAndDevice", (_, ret: { socket: spotifysocket }) => {
+      if (ret) ret.socket.isPremium = true;
       return ret;
     });
   }
